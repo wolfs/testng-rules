c003bdc23b454ba7f18640ffb60196687be2fcba
Stefan Wolf <wolfs@fs.tum.de>
2011-06-10 20:00:30 +0200

    Added README

---
+++ README.md
@@ -0,0 +1,51 @@
+TestNG Rule Support
+===================
+
+About
+-----
+
+This is a prototype of a Rule (like in JUnit Rules) implementation for TestNG.
+
+You need to annotate your test class with @Listeners(RulesListener.class) to activate rule support.
+Then any public field of the class of type IHookable or ITestListener will be executed at the appropriate
+time. Multiple IHookables will be chained.
+
+Example
+-------
+
+Consider the following test:
+
+    @Listeners(RulesListener.class)
+    public class RuleTest {
+        private int counter = 0;
+
+        @TestNGRule
+        public final IHookable incrementRule = new IHookable() {
+
+            @Override
+            public void run(IHookCallBack callBack, ITestResult testResult) {
+                counter += 2;
+                callBack.runTestMethod(testResult);
+            }
+        };
+
+        @Test
+        public void checkItWorked() {
+            System.out.println("Test");
+            assertEquals(counter, 2);
+        }
+    }
+    
+The rule will then be executed (like an IHookable) instead of each method.
+
+In src/test there is also a SpringRule which works like using the AbstractTestNGSpringContextTests as
+a base class, but using composition (with Rules) instead of inheritance. An class then is the SpringRuleTest.
+
+I also implement the ConcurrentRule from [ConcurrentRule].
+
+TODO
+----
+
+Implement classes implementing ITestListener for some common usecases like before/afterClass, ...
+    
+[ConcurrentRule]: http://blog.mycila.com/2009/11/writing-your-own-junit-extensions-using.html
--- src/test/java/com/tngtech/testng/rules/RuleTest.java
+++ src/test/java/com/tngtech/testng/rules/RuleTest.java
@@ -5,8 +5,6 @@ import static org.testng.Assert.*;
 
 import org.testng.IHookCallBack;
 import org.testng.IHookable;
-import org.testng.ITestContext;
-import org.testng.ITestListener;
 import org.testng.ITestResult;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
@@ -16,7 +14,6 @@ import com.tngtech.testng.rules.annotations.TestNGRule;
 @Listeners(RulesListener.class)
 public class RuleTest {
     private int counter = 0;
-    private int counter2 = 0;
 
     @TestNGRule
     public final IHookable incrementRule = new IHookable() {
@@ -28,45 +25,10 @@ public class RuleTest {
         }
     };
 
-    @TestNGRule
-    public IHookable incrementRule2 = new IHookable() {
-
-        @Override
-        public void run(IHookCallBack callBack, ITestResult testResult) {
-            counter *= 3;
-            callBack.runTestMethod(testResult);
-        }
-    };
-
-    @TestNGRule
-    public ITestListener addOne = new TestWatchman() {
-        @Override
-        public void beforeMethod(ITestResult result) {
-        }
-
-        @Override
-        public void onStart(ITestContext context) {
-            counter2 +=1;
-        }
-
-        @Override
-        public void afterMethod(ITestResult result) {
-        }
-
-    };
-
     @Test
     public void checkItWorked() {
         System.out.println("Test");
-        assertEquals(counter, 6);
-        assertEquals(counter2,1);
-    }
-
-    @Test
-    public void checkItWorkedTwice() {
-        System.out.println("Test");
-        assertEquals(counter, 24);
-        assertEquals(counter2,1);
+        assertEquals(counter, 2);
     }
 
 }
